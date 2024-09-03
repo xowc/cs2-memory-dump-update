205a68aebd1b75bb684dd6a5756662f8d7d67291
xowc <lolzuel2@gmail.com>
2024-09-02 23:36:13 -0400

    Add files via upload
    	float x, y, z;

---
+++ host_dll.cs
@@ -0,0 +1,20 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2024-09-03 03:28:22.173057700 UTC
+
+namespace CS2Dumper.Schemas {
+    // Module: host.dll
+    // Class count: 2
+    // Enum count: 0
+    public static class HostDll {
+        // Parent: CAnimScriptBase
+        // Field count: 1
+        public static class EmptyTestScript {
+            public const nint m_hTest = 0x10; // 
+        }
+        // Parent: None
+        // Field count: 1
+        public static class CAnimScriptBase {
+            public const nint m_bIsValid = 0x8; // 
+        }
+    }
+}
